d7e5baf2dc28fda10dd12e4ec7b53fd7497d9451
Raghav Singh Chauhan <raghav14082@iiitd.ac.in>
2023-06-27 18:41:00 -0400

    Update annotate.js

--- annotate.js
+++ annotate.js
@@ -8,7 +8,7 @@ var currentImage = ""
 //var base_app_url = "http://localhost:5000"
 //var base_app_url = "https://64da-38-95-255-16.ngrok-free.app"
 //var base_app_url = "https://8386-128-205-33-151.ngrok.io" // my server ngrok
-var base_app_url = "https://3304-128-205-33-151.ngrok.io" // local ngrok
+var base_app_url = "https://80d5-128-205-33-151.ngrok.io" // local ngrok
 var items_list = []
 
 var sample_labels = [] //["Apple", "Banana", "Mango", "Orange"]
